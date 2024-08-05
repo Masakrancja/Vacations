ced7766e23429a88e56b8ecb298977f1a85afd1b
Piotr Pietrzak <piotrpietrzak145@gmail.com>
2024-08-05 15:38:56 +0200

    feat: events pages for admin

--- src/components/content/adminContent/AdminContent.js
+++ src/components/content/adminContent/AdminContent.js
@@ -5,6 +5,9 @@ import AdminStoreProvider from "./AdminStoreProvider";
 import UsersPage from "./pages/usersPage/UsersPage";
 import UsersPendingPage from "./pages/usersPendingPage/UsersPendingPage";
 import EventsPage from "./pages/eventsPage/EventsPage";
+import EventsPendingPage from "./pages/eventsPendingPage/EventsPendingPage";
+import EventsApprovedPage from "./pages/eventsApprovedPage/EventsApprovedPage";
+import EventsCancelledPage from "./pages/eventsCancelledPage/EventsCancelledPage";
 import AccountPage from "./pages/accountPage/AccountPage";
 import ErrorPage from "../errorPage/ErrorPage";
 
@@ -17,9 +20,9 @@ const AdminContent = () => {
           <Route path="/users" Component={UsersPage} />
           <Route path="/users/pending" Component={UsersPendingPage} />
           <Route path="/events" Component={EventsPage} />
-          {/*<Route path="/events/pending" Component={} />
-          <Route path="/events/approved" Component={} />
-          <Route path="/events/cancelled" Component={} /> */}
+          <Route path="/events/pending" Component={EventsPendingPage} />
+          <Route path="/events/approved" Component={EventsApprovedPage} />
+          <Route path="/events/cancelled" Component={EventsCancelledPage} />
           <Route path="/me" Component={AccountPage} />
           <Route path="*" Component={ErrorPage} />
         </Routes>
---
+++ src/components/content/adminContent/pages/eventsPendingPage/EventsPendingPage.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState, useEffect } from "react";
+import BemCssModules from "bem-css-modules";
+import { StoreContext } from "../../../../../StoreProvider";
+import { AdminStoreContext } from "../../AdminStoreProvider";
+import { URI } from "../../../../../config";
+import SelectUser from "../../../components/selectUser/SelectUser";
+import Event from "../../../components/event/Event";
+import Error from "../../../components/error/Error";
+
+import { default as UsersStyles } from "./EventsPendingPage.module.scss";
+
+const style = BemCssModules(UsersStyles);
+
+const EventsPendingPage = () => {
+  const { token } = useContext(StoreContext);
+  const { userId } = useContext(AdminStoreContext);
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const options = {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+      };
+      if (userId) {
+        return fetch(URI + "/events?userid=" + userId, options);
+      } else {
+        return fetch(URI + "/events", options);
+      }
+    })()
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.code === 200) {
+          setEvents(data.response);
+          setError(false);
+          setMessage("");
+        } else if (data.code !== 404) {
+          setError(true);
+          setMessage(data.message);
+        }
+      })
+      .catch((err) => {
+        setError(true);
+        setMessage(err.message);
+      });
+  }, [token, userId]);
+
+  const eventContent = events
+    .filter((event) => event.status === "pending")
+    .map((event) => <Event key={event.id} {...event} />);
+
+  return (
+    <section className={style()}>
+      <SelectUser />
+      {error ? <Error message={message} /> : eventContent}
+    </section>
+  );
+};
+export default EventsPendingPage;
